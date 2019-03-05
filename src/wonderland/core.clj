bb7de28706da104d53f0a3083b6fcce4d9d6f48a
Justin <justin@baal-software.com>
2019-03-05 08:21:58 -0700

    Simple function expression that returns an intersection of two sets

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -1,6 +1,12 @@
-(ns wonderland.core)
+(ns wonderland.core
+  (:require [clojure.set :as cs]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; Using function expression just to get some reps in
+(def same-fav-foods (fn [fav-foods1 fav-foods2]
+  (let [food-set-1 (set fav-foods1)
+        food-set-2 (set fav-foods2)
+        common-foods (cs/intersection food-set-1 food-set-2)]
+    (str "Common foods: " common-foods))))
+
+(same-fav-foods [:toast :blueburry :orange] [:orange :toast :pnut-butter])
+
