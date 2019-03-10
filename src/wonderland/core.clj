072eb33eddcfa58a87d52454994395ee85494665
Justin <justin@baal-software.com>
2019-03-10 14:50:14 -0600

    "Return tuple of each value between two collections"

--- src/wonderland/core.clj
+++ src/wonderland/core.clj
@@ -13,3 +13,13 @@
 ;; function that lets you know if a given number is greater than zero
 (def greater-than-zero (fn [num]
                          (< 0 num)))
+
+;; lists to use in butterfly of the day function
+(def butterflies '("Milbert's tortoiseshell" "The Zebra Swallowtail" "Red Admiral Monarch Butterfly" "Karner Blue Butterfly"))
+(def week-days '("Monday" "Tuesday" "Wednesday" "Thursday" "Friday"))
+;; function that returns day of week plus its butterfly
+
+(def butterfly-of-the-day (fn []
+                            (for [day week-days butterfly butterflies]
+                              (str day " is " butterfly " day!")
+                             )))
